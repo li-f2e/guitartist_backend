5fdc11bc675013824159a068cfa3cbfdc610bbdb
yuu-chien <55533267+yuu-chien@users.noreply.github.com>
2019-09-22 21:33:54 +0800

    admin_company_edit和info

--- admin_backend/admin_company_edit_2.php
+++ admin_backend/admin_company_edit_2.php
@@ -13,9 +13,6 @@ $row = $stmt->fetch();
 
 <?php require 'admin__header.php'?>
 
-
-
-<link rel="stylesheet" href="css/company_info.css">
 </head>
 <body>
 <?php require "admin__nav_bar.php";?>
@@ -25,164 +22,169 @@ $row = $stmt->fetch();
       <?php require "admin__left_menu.php";?>
 
 
-        <div class="mainContent">
-            <div class="container-fluid">
-                <div class="row justify-content-center">
-                    <div class="col-10 p-0 mt-5 ml-3 ">
-                        <!-- html馬打這裡 -->
-                        <div class="container">
-                            <div style="margin-top: 2rem;">
-                                <div class="row">
-                                <div class="col">
-                                    <div
-                                    class="alert alert-primary"
-                                    role="alert"
-                                    id="info-bar"
-                                    style="display: none"
-                                    ></div>
-                                </div>
-                                </div>
-                                <div class="container mt-4">
-                                <div class="card" style="width: 50rem;">
-                                    <div class="card-body">
-                                    <h5 class="card-title">廠商資料</h5>
-                                    <form name="form1" onsubmit="return checkForm()">
-                                        <input type="hidden" name="sid" value="<?=$row['sid']?>" />
-                                        <table class="table table-hover">
-                                        <tr>
-                                            <th class="align-middle">電子信箱</th>
-                                            <td>
-                                            <div>
-                                                <?=htmlentities($row['email'])?>
-                                                <a href="#" id="addBtn_1" style="margin-left:309px"><i class="fas fa-plus"></i></a>
-                                            </div>
-
-                                            <input
-                                                type="<?=!empty($row['alt_email']) ? 'text' : 'hidden'?>"
-                                                name="alt_email"
-                                                id="alt_email"
-                                                value="<?=htmlentities($row['alt_email'])?>"
-                                            />
-                                            <a
-                                                href=""
-                                                style="<?=!empty($row['alt_email']) ? 'display:inline-block' : 'display: none'?>"
-                                                id="removeBtn_1"
-                                                ><i class="fas fa-minus"></i
-                                            ></a>
-                                            <small id="alt_emailHelp" class="form-text"></small>
-                                            </td>
-                                            <td></td>
-                                        </tr>
-                                        <tr>
-                                            <th class="align-middle">聯絡電話</th>
-                                            <td>
-                                            <div>
-                                              <input
-                                                  type="text"
-                                                  name="tel"
-                                                  id="tel"
-                                                  value="<?=htmlentities($row['tel'])?>"
-                                              />
-                                              <a href="" id="addBtn_2"><i class="fas fa-plus"></i></a>
-                                            </div>
-                                            <div>
-                                              <input
-                                                  type="<?=!empty($row['alt_tel']) ? 'text' : 'hidden'?>"
-                                                  name="alt_tel"
-                                                  id="alt_tel"
-                                                  value="<?=htmlentities($row['alt_tel'])?>"
-                                              />
-                                              <a
-                                                  href=""
-                                                  style="<?=!empty($row['alt_tel']) ? 'display:inline-block' : 'display: none'?>"
-                                                  id="removeBtn_2"
-                                                  ><i class="fas fa-minus"></i
-                                              ></a>
-                                            </div>
-                                            </td>
-                                            <td></td>
-                                        </tr>
-                                        <tr>
-                                            <th class="align-middle">傳真號碼</th>
-                                            <td>
-                                            <input
-                                                type="text"
-                                                name="fax"
-                                                id="fax"
-                                                value="<?=htmlentities($row['fax'])?>"
-                                            />
-                                            </td>
-                                            <td></td>
-                                        </tr>
-                                        <tr>
-                                            <th class="align-middle">聯絡地址</th>
-                                            <td>
-                                            <input
-                                                type="text"
-                                                name="addr"
-                                                id="addr"
-                                                value="<?=htmlentities($row['addr'])?>"
-                                            />
-                                            </td>
-                                            <td></td>
-                                        </tr>
-                                        </table>
-
-                                        <div class="ml-auto d-flex" style="width:130px">
-                                            <button type="submit" class="btn btn-primary" id="submit_btn" style="margin-right:5px">修改</button>
-                                            <a name="" id="" class="btn btn-primary" href="company_info.php" role="button">返回</a>
-                                        </div>
-                                    </form>
-                                    </div>
-                                </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+      <div class="mainContent mainContent-css">
+      <div class="container-fluid">
+        <div class="row justify-content-center">
+          <div class="col-11 mt-5 mx-auto">
+            <!-- html碼打這裡 -->
+            <div class="container">
+              <div style="margin-top: 2rem;">
+                <div class="row">
+                  <div class="col">
+                    <div class="alert alert-primary" role="alert" id="info-bar" style="display: none"></div>
+                  </div>
                 </div>
+
+                <!-- 麵包屑 -->
+                <nav aria-label="breadcrumb">
+                  <ol class="breadcrumb mb-3 p-1">
+                    <li class="breadcrumb-item"><a href="admin_company_list.php">代理商列表</a></li>
+                    <li class="breadcrumb-item"><a href="admin_company_info.php">編輯代理商資訊</a></li>
+                    <li class="breadcrumb-item active font-weight-bold" aria-current="page">編輯聯絡資訊</li>
+                  </ol>
+                </nav>
+                <div class="card edit_card">
+                  <div class="card-body">
+                    <h5 class="card-title">聯絡資訊</h5>
+                    <form name="form1" onsubmit="return checkForm()">
+                      <input type="hidden" name="sid" value="<?=$row['sid']?>" />
+                      <table class="table table-hover">
+                        <tr>
+                          <th class="align-middle">電子信箱</th>
+                          <td>
+                            <div>
+                              <?=htmlentities($row['email'])?>
+                              <a href="#" id="addBtn_1" style="margin-left:309px"><i class="fas fa-plus"></i></a>
+                            </div>
+
+                            <input type="<?=!empty($row['alt_email']) ? 'text' : 'hidden'?>" name="alt_email"
+                              id="alt_email" class="mt-1" value="<?=htmlentities($row['alt_email'])?>" />
+                            <a href="" style="<?=!empty($row['alt_email']) ? 'display:inline-block' : 'display: none'?>"
+                              id="removeBtn_1"><i class="fas fa-minus"></i></a>
+                            <small id="alt_emailHelp" class="form-text"></small>
+                          </td>
+                          <td></td>
+                        </tr>
+                        <tr>
+                          <th class="align-middle">聯絡電話</th>
+                          <td>
+                            <div>
+                              <input type="text" name="tel" id="tel" value="<?=htmlentities($row['tel'])?>" />
+                              <a href="" id="addBtn_2"><i class="fas fa-plus"></i></a>
+                            </div>
+                            <div>
+                              <input type="<?=!empty($row['alt_tel']) ? 'text' : 'hidden'?>" name="alt_tel" id="alt_tel" class="mt-1"
+                                value="<?=htmlentities($row['alt_tel'])?>" />
+                              <a href="" style="<?=!empty($row['alt_tel']) ? 'display:inline-block' : 'display: none'?>"
+                                id="removeBtn_2"><i class="fas fa-minus"></i></a>
+                            </div>
+                          </td>
+                          <td></td>
+                        </tr>
+                        <tr>
+                          <th class="align-middle">傳真號碼</th>
+                          <td>
+                            <input type="text" name="fax" id="fax" value="<?=htmlentities($row['fax'])?>" />
+                          </td>
+                          <td></td>
+                        </tr>
+                        <tr>
+                          <th class="align-middle">聯絡地址</th>
+                          <td>
+                            <input type="text" name="addr" id="addr" value="<?=htmlentities($row['addr'])?>" />
+                          </td>
+                          <td></td>
+                        </tr>
+                      </table>
+
+                      <div class="mr-auto mt-4 d-flex" style="width:130px">
+                        <button type="submit" class="btn btn-dark" id="submit_btn" style="margin-right:5px">修改</button>
+                        <a name="" id="" class="btn btn-light" href="admin_company_info.php" role="button">返回</a>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
             </div>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
+  </div>
 
 <script>
-// email + -
-  let addBtn_1 = document.querySelector("#addBtn_1");
-  let removeBtn_1 = document.querySelector("#removeBtn_1");
-  let input_1 = document.querySelector("#alt_email");
-  addBtn_1.addEventListener("click", function(e) {
-    e.preventDefault();
-    input_1.setAttribute("type", "text");
-    removeBtn_1.style.display = "inline-block";
-  });
-  removeBtn_1.addEventListener("click", function(e) {
-    if (confirm("確定要刪除這筆資料嗎?")) {
-      let alt_emailHelp = document.querySelector("#alt_emailHelp");
-      e.preventDefault();
-      input_1.setAttribute("type", "hidden");
-      removeBtn_1.style.display = "none";
-      input_1.value = "<?=$row['alt_email'] = ''?>";
-      // input_1.style.display = "none";
-      alt_emailHelp.style.display = "none";
-    }
-  });
-  // tel + -
-  let addBtn_2 = document.querySelector("#addBtn_2");
-  let removeBtn_2 = document.querySelector("#removeBtn_2");
-  let input_2 = document.querySelector("#alt_tel");
-  addBtn_2.addEventListener("click", function(e) {
-    e.preventDefault();
-    input_2.setAttribute("type", "text");
-    removeBtn_2.style.display = "inline-block";
-  });
+    // email + -
+    // let addBtn_1 = document.querySelector("#addBtn_1");
+    // let removeBtn_1 = document.querySelector("#removeBtn_1");
+    // let input_1 = document.querySelector("#alt_email");
+    // addBtn_1.addEventListener("click", function (e) {
+    //   e.preventDefault();
+    //   input_1.setAttribute("type", "text");
+    //   removeBtn_1.style.display = "inline-block";
+    // });
+    // removeBtn_1.addEventListener("click", function (e) {
+    //   if (confirm("確定要刪除這筆資料嗎?")) {
+    //     let alt_emailHelp = document.querySelector("#alt_emailHelp");
+    //     e.preventDefault();
+    //     input_1.setAttribute("type", "hidden");
+    //     removeBtn_1.style.display = "none";
+    //     input_1.value = "<?=$row['alt_email'] = ''?>";
+    //     // input_1.style.display = "none";
+    //     alt_emailHelp.style.display = "none";
+    //   }
+    // });
 
-  removeBtn_2.addEventListener("click", function(e) {
-    if (confirm("確定要刪除這筆資料嗎?")) {
-      e.preventDefault();
-      input_2.setAttribute("type", "hidden");
-      removeBtn_2.style.display = "none";
-      input_2.value = "<?=$row['alt_tel'] = ''?>";
-    }
-  });
+    //--------
+    // email + -　用jQuery寫＆加動畫
+    $("#addBtn_1").click(function (e) {
+      e.preventDefault()
+      $("#alt_email").removeClass("animated fadeOutUp").addClass("animated fadeInDown mt-1").attr("type", "text")
+      $("#removeBtn_1").show()
+    })
+
+    $("#removeBtn_1").click(function (e) {
+      e.preventDefault()
+      confirm("確定要刪除這筆電子信箱資料嗎？")
+      $("#alt_email").removeClass("animated fadeInDown").addClass("animated fadeOutUp").attr("type", "hidden").attr("value","<?=$row['alt_email'] = ''?>")
+      $(this).hide()
+    })
+
+    // tel + -
+    // let addBtn_2 = document.querySelector("#addBtn_2");
+    // let removeBtn_2 = document.querySelector("#removeBtn_2");
+    // let input_2 = document.querySelector("#alt_tel");
+    // addBtn_2.addEventListener("click", function (e) {
+    //   e.preventDefault();
+    //   input_2.setAttribute("type", "text");
+    //   removeBtn_2.style.display = "inline-block";
+    // });
+
+    // removeBtn_2.addEventListener("click", function (e) {
+    //   if (confirm("確定要刪除這筆資料嗎?")) {
+    //     e.preventDefault();
+    //     input_2.setAttribute("type", "hidden");
+    //     removeBtn_2.style.display = "none";
+    //     input_2.value = "<?=$row['alt_tel'] = ''?>";
+    //   }
+    // });
+    
+    //--------
+    // tel + -　用jQuery寫＆加動畫
+    $("#addBtn_2").click(function (e) {
+      e.preventDefault()
+      $("#alt_tel").removeClass("animated fadeOutUp").addClass("animated fadeInDown mt-1").attr("type", "text")
+      $("#removeBtn_2").show()
+    })
+
+    $("#removeBtn_2").click(function (e) {
+      e.preventDefault()
+      confirm("確定要刪除這筆電話資料嗎？")
+      $("#alt_tel").removeClass("animated fadeInDown").addClass("animated fadeOutUp").attr("type", "hidden").attr("value","<?=$row['alt_tel'] = ''?>")
+      $(this).hide()
+    })
+
+
   //pattern
   const required_fields = [
     {
